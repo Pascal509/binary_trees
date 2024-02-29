29544a325b1cfa9cfa93e6453401c20a85693be5
Pascal509 <chinemeremezenagu@gmail.com>
2024-02-29 14:29:58 +0100

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,13 +15,16 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t leftHeight;
+	size_t rightHeight;
+
 	if (tree == NULL)
 		return (0);
 
-	size_t leftHeight = binary_tree_height(tree->left);
-	size_t rightHeight = binary_tree_height(tree->right);
+	leftHeight = binary_tree_height(tree->left);
+	rightHeight = binary_tree_height(tree->right);
 
-	if (rightHeight > leftHeight)
+	if (rightHeight >= leftHeight)
 		return (rightHeight + 1);
 	else
 		return (leftHeight + 1);
