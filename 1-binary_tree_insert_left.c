d064e7bcba60a63a0b51ecf2bffa3bb72d9d5677
Pascal509 <chinemeremezenagu@gmail.com>
2024-02-29 11:42:30 +0100

    poy

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,23 +15,24 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
-
 	if (parent == NULL)
-		return (NULL);
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
-		return (NULL);
+		return NULL;
+	binary_tree_t *newLeftNode = binary_tree_node(parent, value);
+	if (newLeftNode == NULL)
+		return NULL;
 
+	/* If parent has left node, set as left child of new node*/
 	if (parent->left != NULL)
 	{
-		new_node->left = parent->left;
-		new_node->left->parent = new_node;
-		parent->left = new_node;
+		newLeftNode->left = parent->left;
+		parent->left->parent = newLeftNode;
+		parent->left = newLeftNode;
 	}
 	else
 	{
-		parent->left = new_node;
+		/*Set new Node as left child of the parent*/
+		parent->left = newLeftNode;
 	}
-	return (new_node);
+
+	return (newLeftNode);
 }
